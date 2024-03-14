69cd426131f3ea6638a4cc3b53fa6a1292d547c7
Some0ne3lse <143652507+Some0ne3lse@users.noreply.github.com>
2024-03-14 23:55:30 +0000

    get rid of old scripts

--- scripts.js
+++ scripts.js
@@ -1,21 +1,31 @@
-let number = 1;
+let number = 0;
 
-let inputNumber = 1;
+let inputNumber = 0;
 
-let limitNumber = 1;
+let limitNumber = 0;
 
-let clickNumber = 0;
-
+let savedInput = +localStorage.getItem('Input Number', inputNumber);
+let savedLimit = +localStorage.getItem('Limit number', limitNumber);
 
 let inputValue = document.getElementById('inc-and-dec-input');
 let limitInput = document.getElementById('limit-input');
 
-
 const inputNumberTag = document.getElementById('current-input-number');
 const limitNumberTag = document.getElementById('current-limit-number');
+if (savedInput != 0) {
+  inputNumber = savedInput;
+  inputNumberTag.textContent = (`Current increment/decrement value is ${inputNumber}`);
+}
+
+if (savedLimit != 0) {
+  limitNumber = savedLimit;
+  limitNumberTag.textContent = (`Current boom limit is ${limitNumber}`);
+
+}
+
+
 
 const saveToLocalStorage = () => {
-  clickNumber = 0;
   inputNumber = +inputValue.value;
   limitNumber = +limitInput.value;
   localStorage.setItem('Input Number', inputNumber);
@@ -25,11 +35,8 @@ const saveToLocalStorage = () => {
 }
 
 const getItemFromLocalStorage = () => {
-  inputNumber = 0;
-  limitNumber = 0;
-  clickNumber = 0;
-  const savedInput = +localStorage.getItem('Input Number', inputNumber);
-  const savedLimit = +localStorage.getItem('Limit number', limitNumber);
+  savedInput = +localStorage.getItem('Input Number', inputNumber);
+  savedLimit = +localStorage.getItem('Limit number', limitNumber);
   inputNumber = savedInput;
   limitNumber = savedLimit;
   inputNumberTag.textContent = (`Current increment/decrement value is ${inputNumber}`);
@@ -45,14 +52,13 @@ const isEmpty = (id) => {
 }
 
 const onIncrementClick = () => {
-  number = number * inputNumber;
+  number = number + inputNumber;
   numberTag.textContent = number;
-  ++clickNumber;
-  if ((clickNumber === limitNumber || clickNumber === -Math.abs(limitNumber)) && isEmpty('boom-list')) {
+  if ((number >= limitNumber) && isEmpty('boom-list')) {
     const boomItem = document.createElement('li');
     boomItem.textContent = '!bOoMm!';
     list.appendChild(boomItem);
-  } else if (clickNumber < limitNumber && clickNumber > -Math.abs(limitNumber)) {
+  } else if (number < limitNumber && number > -Math.abs(limitNumber)) {
     while (list.firstChild) {
       list.removeChild(list.firstChild);
     }
@@ -60,14 +66,13 @@ const onIncrementClick = () => {
 }
 
 const onDecrementClick = () => {
-  number = number / inputNumber;
+  number = number - inputNumber;
   numberTag.textContent = number;
-  --clickNumber;
-  if ((clickNumber === limitNumber || clickNumber === -Math.abs(limitNumber)) && isEmpty('boom-list')) {
+  if ((number <= -Math.abs(limitNumber)) && isEmpty('boom-list')) {
     const boomItem = document.createElement('li');
     boomItem.textContent = '!bOoMm!';
     list.appendChild(boomItem);
-  } else if (clickNumber < limitNumber && clickNumber > -Math.abs(limitNumber)) {
+  } else if (number < limitNumber && number > -Math.abs(limitNumber)) {
     while (list.firstChild) {
       list.removeChild(list.firstChild);
     }
